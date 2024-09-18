66800ee22051be8130e09dd05cc8db02350e5b8a
gaspar <gaspar.henniaux.etu@univ-lille.fr>
2024-09-18 23:38:08 +0200

    correction questions 4 et 5

--- tp1/verif.py
+++ tp1/verif.py
@@ -16,38 +16,47 @@ def uncompact_write(tuples):
         uncompact.append((tuples[i][0], height))
         uncompact.append(tuples[i])
         height = tuples[i][1]
-
     return uncompact
 
-folder_path = os.path.dirname(os.path.abspath(__file__)) + "/files"  
+def get_files(folder_path):
+    filenames = []
+    for d, _, fs in os.walk(folder_path):
+        for f in fs:
+            filenames.append(os.path.join(d, f))
+    input_files = [f for f in filenames if f.endswith("input.txt")]
+    output_files = [f for f in filenames if f.endswith("output.txt")]
+    input_files.sort()
+    output_files.sort()
+    ids = [f.split("-")[6] for f in input_files]
+    return input_files, output_files, ids
 
-filenames = []
-for d, _, fs in os.walk(folder_path):
-    for f in fs:
-        filenames.append(os.path.join(d, f))
-input_files = [f for f in filenames if f.endswith("input.txt")]
-output_files = [f for f in filenames if f.endswith("output.txt")]
-input_files.sort()
-output_files.sort()
-ids = [f.split("-")[6] for f in input_files]
+def process_files(input_files, output_files, ids):
+    for i in range(len(input_files)):
+        buildings = []
+        with open(input_files[i], "r") as f:
+            n = int(f.readline())
+            for j in range(n):
+                buildings.append(tuple(map(int, f.readline().split())))
+        roof_line = algo.building_to_roof_line(buildings)
+        with open(output_files[i], "r") as f:
+            expected = [eval(item) for item in f.read().strip().split("\n")]
+            roof_line = uncompact_write(roof_line)
+            print("ID: ", ids[i], "\n")
+            print("Expected: ", expected, "\n")
+            print("Output: ", roof_line, "\n")
+            if roof_line == expected:
+                print("Test passed " + "\033[92m✅\033[0m" + "\n")
+            else:
+                print("Test failed " + "\033[91m❌\033[0m" + "\n")
+            print("=====================================")
 
-for i in range(len(input_files)):
-    buildings = []
-    with open(input_files[i], "r") as f:
-        n = int(f.readline())
-        for j in range(n):
-            buildings.append(tuple(map(int, f.readline().split())))
-    roof_line = algo.building_to_roof_line(buildings)
-    with open(output_files[i], "r") as f:
-        expected = f.read().strip().split("\n")
-        expected = [eval(item) for item in expected]
-        expected = compact_write(expected)
-        # roof_line = uncompact_write(roof_line)
-        if len(roof_line) < 20:
-            print("ID: ", ids[i])
-            print("Expected: ", expected)
-            print("Output: ", roof_line)
-            print("\n")
+def main():
+    folder_path = os.path.dirname(os.path.abspath(__file__)) + "/files"
+    input_files, output_files, ids = get_files(folder_path)
+    process_files(input_files, output_files, ids)
+
+if __name__ == "__main__":
+    main()
             
 
 
